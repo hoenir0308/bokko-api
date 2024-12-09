3cf830ee26ed6768beabd4a58f6b50772b1984b5
IEnumerablee <thepashalza@gmail.com>
2024-12-09 14:14:50 +0500

    changing bot token

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 DATABASE_URL = "mongodb://mongo:27017/"
 DATABASE_NAME = "bokko_bot"
 
-TELEGRAM_TOKEN = "7481839741:AAH6ESm8ZVrY48JHWQl5Z59X_ReXHdTFhbQ"
+TELEGRAM_TOKEN = "7699691572:AAEA7LgOUgKOajgjln9_ihwkQouvsYh0HGo"
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/'
 
 GEMINI_API_KEY = "AIzaSyCB6zpqVYvx-uoLclQuAfygopvx8ki7iDc"
