cd333240055113bdff81c6ef669afc9d84bd82c1
IEnumerablee <thepashalza@gmail.com>
2024-12-13 16:47:28 +0500

    Groq xd

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ DATABASE_NAME = "bokko_bot"
 TELEGRAM_TOKEN = "7699691572:AAEA7LgOUgKOajgjln9_ihwkQouvsYh0HGo"
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/'
 
-GEMINI_API_KEY = "AIzaSyCB6zpqVYvx-uoLclQuAfygopvx8ki7iDc"
+GROQ_API_KEY = "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"
 
--- src/utils/promt.py
+++ src/utils/promt.py
@@ -1,18 +1,36 @@
-from data.gemini import Gemini
-from config import GEMINI_API_KEY
+import openai
+from config import GROQ_API_KEY
 
-gemini = Gemini(
-    key=GEMINI_API_KEY
+client = openai.OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=GROQ_API_KEY
 )
 
 async def ask_decomposing(title: str, text: str, name: str, age: int) -> str:
-    promt_text = f"Привет, ты самый лучший персональный тьютор, можешь декомпозировать цель: {title} и расписать возможные ее задачи вот ее описание {text}. Меня зовут {name}, мне {age} лет. Отвечай эмоционально и можешь использовать эмодзи."
-    result = await gemini.ask(promt_text)
-    return result
+    promt_text = f"Ты самый лучший персональный тьютор для {name}, возраста {age}. Твоя цель - декомопозировать задачи. Отвечай эмоционально. Так же, ты можешь использовать эмодзи."
+    
+    result = openai.ChatCompletion.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "system", "content": promt_text},
+            {"role": "user", "content": f"{title} \n {text}"},
+        ],
+    )
 
-async def ask_motivation(title: str, tasks: list):
-    promt_text = f"Привет, ты самый лучший персональный тьютор, можешь задать мотивацию для выполнение цели: {title}. Вот задачи, которе сгорают скоро:"
+    return result["choices"][0]["message"]["content"]
+
+async def ask_motivation(title: str, tasks: list, name: str, age: int):
+    promt_text = f"Ты самый лучший персональный тьютор. Твоя задача - мотивировть {name}, возрастом {age}, для выполнения цели {title}, состоящей из множества подцелей"
+    tasks_prompt = ""
     for task in tasks:
         promt_text += f"\n {str(task)}"
-    result = await gemini.ask(promt_text)
-    return result
+    
+    result = openai.ChatCompletion.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "system", "content": promt_text},
+            {"role": "user", "content": tasks_prompt},
+        ],
+    )
+
+    return result["choices"][0]["message"]["content"]
